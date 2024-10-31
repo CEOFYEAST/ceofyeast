a6a798f3b7df4cc27a619976d8b96b8d0c0801e5
CEOFYEAST <benton.diebold@me.com>
2024-10-31 08:09:34 -0700

    built project

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "/",
+  publicPath: process.env.NODE_ENV = "/",
   transpileDependencies: true
 })
