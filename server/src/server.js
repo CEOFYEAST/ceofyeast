b971e19aa17f09e138b830d1fb7051bd475d05f3
CEOFYEAST <benton.diebold@me.com>
2024-11-06 15:26:07 -0800

    Added initial contents of server directory

---
+++ server/src/server.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+'use strict'
+
+// Read the .env file.
+//require('dotenv').config()
+
+// Require the framework
+const Fastify = require('fastify')
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+  pluginTimeout: 10000
+})
+
+app.register(require('@fastify/cors'), {})
+app.register(require('@fastify/swagger'), {})
+app.register(require('@fastify/swagger-ui'), {})
+app.register(require('./routes/items.js'))
+
+const PORT = process.env.PORT || 3001;
+//const HOST = 'factorio-production-calculator.com'
+const HOST = 'localhost'
+
+// Start listening.
+app.listen({ port: PORT, host: HOST}, (err, address) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    console.log(`Fastify server is running on ${address}`);
+  });
